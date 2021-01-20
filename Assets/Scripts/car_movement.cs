10ce84f75f9fe4c9788e3fe0bbdbf43cc97586c5
Alperen <enesalperenbayar@gmail.com>
2021-01-20 17:55:11 +0300

    Basic Car Movement Added
    --> car_movement.cs added. Now car can move around the map.
    --> Cinemachine package added. Now virtual camera is following the car.

---
+++ Assets/Scripts/car_movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_movement : MonoBehaviour
+{
+    Rigidbody2D rigidbodyCar; // Referance for car's rigidbody
+
+    [SerializeField]
+    float car_speed = 15f; // Car's frontal speed
+    [SerializeField]
+    float steering_speed = 1f; // Car's steering speed
+    float total_steering, direction; // Values for calculating direction vector
+
+
+
+
+
+    void Start()
+    {
+        rigidbodyCar = GetComponent<Rigidbody2D>(); // Initialization for car's rigidbody
+    }
+
+    /* I'm using FixedUpdate() instead of Update() method for car movement, because: 
+     *
+     * "It's for this reason that FixedUpdate should be used when applying forces,
+     * torques, or other physics-related functions - because you know it will 
+     * be executed exactly in sync with the physics engine itself."
+     * 
+     * - From Unity Forms (https://answers.unity.com/questions/10993/whats-the-difference-between-update-and-fixedupdat.html)
+     * 
+    */
+    void FixedUpdate()
+    {
+        // Automatic drives front direction
+        rigidbodyCar.AddRelativeForce(Vector2.up * car_speed);
+
+        // Right and left steering
+        direction = Mathf.Sign(Vector2.Dot(rigidbodyCar.velocity, rigidbodyCar.GetRelativeVector(Vector2.up)));
+        total_steering = -Input.GetAxis("Horizontal");
+        rigidbodyCar.rotation += total_steering * steering_speed * rigidbodyCar.velocity.magnitude * direction;
+        rigidbodyCar.AddRelativeForce(-Vector2.right * rigidbodyCar.velocity.magnitude * total_steering / 2);
+
+    }
+}
